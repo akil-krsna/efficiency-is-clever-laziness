9e0b4210ba27cf29eb5d8eccb977c0ca8dc55a23
adhit-420 <simadi2004@gmail.com>
2023-04-30 02:04:11 +0530

    doccompleted

--- eicl.py
+++ eicl.py
@@ -7,20 +7,34 @@ if sys.argv[2]:
     submodule = sys.argv[2]
 root_help = """Syntax
 python eicl module_name submodule_name
-available modules : cpu_gpu, mixed, io
+available modules : cpu_gpu, mixed
 type python eicl module_name help for help"""
 if __name__ == "__main__":
     if module.lower() == "cpu_gpu":
-        if submodule == "help":
-            print("""available submodules : blender""")
+        if submodule =="help":
+            print('''
+Usage:
+    powerbench [options]
+
+Options:
+    help           Show this help message and exit
+
+
+Description:
+    This is a command-line application for benchmarking power consumption and efficiency. 
+    The application supports benchmarking of CPU and GPU. Results can be written to an output file.
+    Consolidated efficiency score is printed onto stdout.
+
+Examples:
+   #It triggers a script respective module and submodule
+   python eicl.py cpu_gpu
+
+   #Benchmark nvidia GPU 
+    python eicl.py cpu_gpu pycuda
+   ''')
         if submodule.lower() == "blender":
             cpu_gpu.blender.main()
     elif module == "mixed":
         ...
-    elif module == "io":
-        ...
-    elif module == "help":
-        if submodule == "":
-            print(root_help)
     else:
         print(root_help)
